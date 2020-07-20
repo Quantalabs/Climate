1577c51646e36b23aed20118ed3f2f182c998c2f
QLabs <55121845+Quantalabs@users.noreply.github.com>
2020-07-19 17:48:33 -0700

    Add files via upload

---
+++ Climate.py
@@ -0,0 +1,20 @@
+file = open("Global Temperatures 1880-2020.csv", "r")
+data = file.readlines()
+
+years = []
+for line in data:
+    years.append(-int(line.split(',')[0]))
+
+temp = []
+for line in data:
+    numlist = line.split(',')[1].split()
+    num = float(''.join(numlist))
+    temp.append(num)
+
+
+from matplotlib import pyplot as plt
+
+plt.plot(years,temp)
+plt.ylabel('temperature (C)')
+plt.xlabel('years')
+plt.show()
