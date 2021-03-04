b032dd43143cfad33eef048591c8d30ac8ae74c4
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-03-04 14:58:36 -0800

    All Imports go at Top of Python File

--- Climate.py
+++ Climate.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 file = open("Global Temperatures 1880-2020.csv", "r")
 data = file.readlines()
 
@@ -10,9 +12,6 @@ for line in data:
     numlist = line.split(',')[1].split()
     num = float(''.join(numlist))
     temp.append(num)
-
-
-from matplotlib import pyplot as plt
 
 plt.plot(years,temp)
 plt.ylabel('temperature (C)')
