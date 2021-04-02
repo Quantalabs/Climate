8ee486ca9eeaee3604b4bebe4d1a30b76d12703c
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2021-04-01 19:18:06 -0700

    Create getter.py
    Gets climate data from URL at https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt
    This ensures up-to-date and reliable information downloaded for each year since 1880 from NASA.

---
+++ getter.py
@@ -0,0 +1,28 @@
+import urllib
+
+URL = 'https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt'
+"""
+Land-Ocean Temperature Index (C)
+--------------------------------
+
+Year No_Smoothing  Lowess(5)
+----------------------------
+1880     -0.16     -0.08
+1881     -0.07     -0.12
+1882     -0.10     -0.16
+yyyy     (-)#.##   (-)#.##
+"""
+
+file = urllib.request.urlopen(url)
+decoded_file = []
+
+for line in file[5:]:
+	decoded_line = line.decode("utf-8")
+	decoded_file.append(decoded_line)
+  
+tsv_save = open('data/nasa-climate-data.tsv', 'w+')
+tsv_save.writelines(decoded_file)
+
+print('Process complete: ')
+print('Data obtained from nasa.gov and saved to .tsv')
+print('See data/nasa-climate-data.tsv')
